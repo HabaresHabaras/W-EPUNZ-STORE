eb076b1101ce5bd6e48bd5ef552a5fcc38cc98ad
Dali Gala <habaresbit@gmail.com>
2018-10-20 04:32:17 -0400

    new order confirmation

--- src/components/Orders/OrderConfirmation.js
+++ src/components/Orders/OrderConfirmation.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import * as WeLoveYou from '../../assets/img/weloveyou.svg';
 
 const OrderConfirmation = () => (
   <main role="main" id="container" className="main-container push">
     <section className="order-confirmation">
       <div className="content">
         <div className="confirmation">
-          <h2>Awesome, your order has been placed</h2>
-          <p>Make sure you check your emails for confirmation.</p>
-          <img src={WeLoveYou} alt="We Love You" />
+          <h2>All done, your order was placed!</h2>
+          <p>You'll get a confirmation in your email</p>
+          
         </div>
       </div>
     </section>
